550d20ebd669a14fa060e1717fa7b2839e2d6538
SRIRAM <69300593+dev-sriramp@users.noreply.github.com>
2023-06-08 08:36:25 +0000

    Added Ke Prop

--- src/Layout.js
+++ src/Layout.js
@@ -7,9 +7,9 @@ return (
     <div>
         <div className="container-xxl d-flex flex-column border rounded mt-3" style={{height: "83vh"}}>
             {props.layout.map((value, rowindex) => (
-                <div key={rowindex + 1} className="row  flex-fill">
+                <div key={`rowindex${rowindex}`} className="row  flex-fill">
                     {value.map((value, colindex) => (
-                        <Plot rowIndex={rowindex} colIndex={colindex} value={value}/>
+                        <Plot key={colindex} rowIndex={rowindex} colIndex={colindex} value={value}/>
                     ))}
                 </div>
             ))}
